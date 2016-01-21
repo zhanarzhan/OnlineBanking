b8b669976a66e96ed04039236ca86467ce27674c
Zhanarzhan Yelubayeva <zhanarzhan@users.noreply.github.com>
2016-01-21 16:51:05 -0600

    Create TransferException.java

---
+++ exceprion/TransferException.java
@@ -0,0 +1,12 @@
+
+package bankapp.exception;
+
+public class TransferException extends Exception {
+
+    public TransferException () { }
+
+    public TransferException (String msg) {
+        super(msg);
+    } 
+}
+
