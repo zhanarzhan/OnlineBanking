9bf7b64d33d562c60008d33ac5e2e3c814a6b147
Zhanarzhan Yelubayeva <zhanarzhan@users.noreply.github.com>
2016-01-21 16:46:19 -0600

    Create DateException.java

---
+++ exceprion/DateException.java
@@ -0,0 +1,11 @@
+
+package bankapp.exception;
+
+public class DateException extends Exception {
+
+    public DateException () { }
+
+    public DateException (String msg) {
+        super(msg);
+    } 
+}
